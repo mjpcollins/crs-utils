6aaf95436fe1571ec8b474497e044ce2b34a36de
Matt Collins <matthewjpcollins@gmail.com>
2021-07-11 15:10:40 +0100

    remove print statments and set to version 1.0.0

--- src/crs_utils/ps.py
+++ src/crs_utils/ps.py
@@ -1,7 +1,6 @@
 import os
 import zlib
 import json
-from concurrent import futures
 from google.cloud import pubsub_v1
 
 
@@ -9,29 +8,12 @@ def get_gcp_project():
     return os.getenv('GCP_PROJECT')
 
 
-def post_to_pubsub(data, topic, compress=False, callback=False):
+def post_to_pubsub(data, topic, compress=False):
     publisher = pubsub_v1.PublisherClient()
     project = get_gcp_project()
     topic_path = publisher.topic_path(project, topic)
-    publish_futures = []
     message = get_pubsub_safe_message(data, compress)
-    publish_future = publisher.publish(topic_path, message)
-    if callback:
-        publish_future.add_done_callback(get_callback(publish_future, data))
-        publish_futures.append(publish_future)
-        futures.wait(publish_futures, return_when=futures.ALL_COMPLETED)
-        print(f"Published messages with error handler to {topic_path}.")
-
-
-def get_callback(publish_future, data):
-    def callback(publish_future):
-        try:
-            # Wait 60 seconds for the publish call to succeed.
-            print(publish_future.result(timeout=60))
-        except futures.TimeoutError:
-            print(f"Publishing {data} timed out.")
-
-    return callback
+    publisher.publish(topic_path, message)
 
 
 def get_pubsub_safe_message(data, compress=False):
