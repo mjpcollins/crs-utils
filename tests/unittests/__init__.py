3cc953a93e13057cbc4d2da58e3ad6fcee650faa
Matt Collins <matthewjpcollins@gmail.com>
2021-07-11 12:19:54 +0100

    setup file and folder structure

---
+++ tests/unittests/__init__.py
@@ -0,0 +1,1 @@
+from tests.unittests.test_example import TestExample
---
+++ tests/unittests/test_example.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+
+class TestExample(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_init(self):
+        pass
